611a87dfb3d7df27a99409b92422394281bb7de0
David Vergaray <david.vergaray.a@gmail.com>
2022-04-29 14:05:43 -0400

    Add method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts, class_name: 'Post'
   has_many :likes, class_name: 'Like'
   has_many :comments, class_name: 'Comment'
+
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
