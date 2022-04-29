81959439d2a182cab2121855e15d3060743482da
David Vergaray <david.vergaray.a@gmail.com>
2022-04-29 14:25:08 -0400

    Add method to Like Model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ActiveRecord::Base
   belongs_to :user, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+  def update_post_like_counter
+    if post.likes_counter.nil?
+      post.update(likes_counter: 1)
+    else
+      post.update(likes_counter: post.likes_counter + 1)
+    end
+  end
 end
