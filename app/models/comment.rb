71f4250ec89413cd07cbcff788770e2f2d940e14
David Vergaray <david.vergaray.a@gmail.com>
2022-04-29 14:23:38 -0400

    Add methods to Comment Model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+  def update_post_comment_counter
+    if post.comments_counter.nil?
+      post.update(comments_counter: 1)
+    else
+      post.update(comments_counter: post.comments_counter + 1)
+    end
+  end
 end
