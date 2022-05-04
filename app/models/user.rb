2f140b136bbf3b4c8f5b8cee9d6e2fd38ce2acdc
David Vergaray <david.vergaray.a@gmail.com>
2022-05-04 15:15:20 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_posts
     posts.limit(3).order(created_at: :desc)
   end
