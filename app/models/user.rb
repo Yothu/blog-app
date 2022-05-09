b49e767d0d1dbc21461bc98e719d010ab92d47fe
David Vergaray <david.vergaray.a@gmail.com>
2022-05-09 12:12:29 -0400

    Generate devise User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id', class_name: 'Post'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
