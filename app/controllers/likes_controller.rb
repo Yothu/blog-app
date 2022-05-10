f8d41b2185c23238e30947930e44f534de9d8575
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-10 23:57:18 +0300

    Update likes counter

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,8 @@ class LikesController < ApplicationController
   load_and_authorize_resource
 
   def create
-    @post = Post.find(params[:id])
-
+    @post = Post.find(params[:post_id])
+    
     return if @post.liked?(current_user)
 
     @like = Like.new(author_id: current_user.id, post_id: @post.id)
