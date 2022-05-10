32bfb48cc6b4ca178eb3202203e7f49a55cf6143
David Vergaray <david.vergaray.a@gmail.com>
2022-05-10 16:05:27 -0400

    Add dependency on cascade to fks

--- db/migrate/20220502194150_add_users_ref_to_likes.rb
+++ db/migrate/20220502194150_add_users_ref_to_likes.rb
@@ -1,5 +1,5 @@
 class AddUsersRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key :likes, :users, column: :author_id
+    add_foreign_key :likes, :users, column: :author_id, on_delete: :cascade
   end
 end
--- db/migrate/20220502194246_add_posts_ref_to_comments.rb
+++ db/migrate/20220502194246_add_posts_ref_to_comments.rb
@@ -1,5 +1,5 @@
 class AddPostsRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key :comments, :posts, column: :post_id
+    add_foreign_key :comments, :posts, column: :post_id, on_delete: :cascade
   end
 end
