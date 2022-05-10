293fdd3016b8a5ebb658cd8a77c59da725a6df2c
David Vergaray <david.vergaray.a@gmail.com>
2022-05-10 15:12:56 -0400

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords'}
-  root to: 'users#index', as: 'users'
+  root to: 'users#index'
 
   devise_scope :user do 
     get '/users/sign_out', to: 'users/sessions#destroy'
   end
-  get '/users/:id', to: 'users#show', as: 'user'
 
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/posts/new', to: 'posts#new', as: 'new_post'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
-  post '/posts', to: 'posts#create'
-  post '/users/:user_id/posts/:id/comments', to: 'comments#create'
-  post '/users/:user_id/posts/:id/likes', to: 'likes#create'
-
-  delete '/users/:user_id/posts/:id' ,to: 'posts#destroy', as: 'destroy_user_post'
-  delete '/users/:user_id/posts/:post_id/comments/:id' ,to: 'comments#destroy', as: 'destroy_user_post_comment'
+  resources :posts, only: [:new, :create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :destroy] do
+      resources :comments, only: [:create, :destroy]
+      resources :likes, only: [:create]
+    end
+  end
 end
