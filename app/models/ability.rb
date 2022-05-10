738d39ba5ff16c061d90dc1d93f8d0096e5c27bf
David Vergaray <david.vergaray.a@gmail.com>
2022-05-10 13:30:57 -0400

    Add permisions to not-admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+    # user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
     else
+      can :manage, Post, author_id: user.id
+      can :manage, Like, author_id: user.id
+      can :manage, Comment, author_id: user.id
       can :read, :all
     end
     #
