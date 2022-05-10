29b670d723488de6834452995b685d99724d7a95
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-10 18:19:54 +0300

    Add a role column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_161208) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_151405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_161208) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter", default: 0, null: false
-    t.integer "likes_counter", default: 0, null: false
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_161208) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter", default: 0, null: false
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -56,9 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_161208) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.string "role", default: "user", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
