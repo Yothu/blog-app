8b246d7cc6f231647217fa985649961c0ca530a6
David Vergaray <david.vergaray.a@gmail.com>
2022-05-11 16:34:05 -0400

    Finish list posts of a user endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ module Api
     class Api::V1::UsersController < ApplicationController
       def show
         @user = User.find(params[:id])
-        render json: @user, status: :ok
+        render json: @user.posts, status: :ok
       end
     end
   end
