76eeb6fb01807bbb949f27fb4fe0d226806a21d4
David Vergaray <david.vergaray.a@gmail.com>
2022-05-11 17:57:02 -0400

    Finish create comment api endpoint

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -17,8 +17,30 @@ module Api
       end
 
       def create
-        
+        comment = Comment.new(comment_params)
+        comment.author_id = params[:user_id]
+        comment.post_id = params[:post_id]
+
+        if comment.save
+          render json: comment, status: :created
+          redirect_to '/api/v1/users'
+        else
+          render json: comment.errors, status: :unproccessable_entity
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:text)
       end
     end
   end
 end
+
+# curl --header "Content-Type: application/json" --request POST
+# --data '{"text": "this is my comment from a curl!!!"}'
+# http://localhost:3000/api/v1/users/1/posts/2/create -v
+
+# curl -v http://localhost:3000/api/v1/users/1/posts/2/comments
+# -X POST -H "Content-type: application/json" -d '{"text": "this is my comment from a curl!!!"}'
