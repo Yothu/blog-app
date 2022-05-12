cc6328c9799bc1a4346d5f2a2df1729d3676e39f
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-13 02:36:40 +0300

    Add authenticate_user_api method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :authenticate_user!
+  # before_action :authenticate_user_api
   protect_from_forgery with: :null_session
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def authenticate_user_api
+    user = User.find_by(email: params[:email])
+
+    if user.correct_password?(params[:token])
+      sign_in(:user, user)
+      # render json: { message: current_user }, status: :ok
+    else
+      render json: { error: 'nah men...' }
+    end
   end
 end
